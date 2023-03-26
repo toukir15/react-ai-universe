17b10ce85a64f37dc796efa1fb9752f4b6ab13c6
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-26 22:00:49 +0600

    added show all button data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
+import Button from "../Button/Button";
 import SingleData from "../SingleData/SingleData";
 
 const Card = () => {
   const [data, setData] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+
+  const handleShowAll = () => {
+    setShowAll(true);
+  };
   useEffect(() => {
     const loadData = async () => {
       const res = await fetch(
@@ -16,9 +22,12 @@ const Card = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto my-5">
-        {data.map((singleData) => (
+        {data.slice(0, showAll ? 12 : 6).map((singleData) => (
           <SingleData singleData={singleData}></SingleData>
         ))}
+      </div>
+      <div onClick={handleShowAll}>
+        <Button>See More</Button>
       </div>
     </>
   );
